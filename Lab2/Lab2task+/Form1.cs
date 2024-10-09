180b0a0d28da08d15fe992c135807826783913f6
Valeria Karpachova <xrd132006lera@gmail.com>
2024-10-09 23:44:02 +0300

    +++

---
+++ Lab2/Lab2task+/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Lab2task_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chart1.Series.Clear();
+
+            double minX = double.Parse(textBoxMinX.Text);
+            double maxX = double.Parse(textBoxMaxX.Text);
+
+          
+            string selectedFunction = comboBox1.SelectedItem.ToString();
+            Series series = new Series
+            {
+                ChartType = SeriesChartType.Line
+            };
+
+            for (double x = minX; x <= maxX; x += 0.1)
+            {
+                double y = 0;
+                switch (selectedFunction)
+                {
+                    case "Parabola":
+                        y = x * x;
+                        break;
+                    case "Sin":
+                        y = Math.Sin(x);
+                        break;
+                    case "Tan":
+                        y = Math.Tan(x); 
+                        break;
+                    case "Hyperbole":
+                        if (x != 0) 
+                        {
+                            y = 1 / x; 
+                        }
+                        break;
+                }
+                series.Points.AddXY(x, y);
+            }
+            chart1.Series.Add(series);
+        }
+    }
+}
+        
+   
